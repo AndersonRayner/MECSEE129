23c8891aa200b8a2d54cb947100ab8a17caf9abb
matt <matthew.anderson@caltech.edu>
2019-11-08 01:05:03 +0000

    Started working on a tmux script to boot everything

---
+++ tmux_scripts/boot_ROS.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+SESSION=dev
+
+# Create the new session
+tmux new -s $SESSION -n "ROS" -d
+
+
+# Split the windows up
+tmux split-window -v -t $SESSION:0
+tmux split-window -h -t $SESSION:0.1
+tmux split-window -h -t $SESSION:0.0
+
+# Send commands
+# C-m is the equivalent of hitting enter
+# Start htop
+tmux send-keys -t $SESSION:0.0 'htop' C-m
+
+# Start ROS
+tmux send-keys -t $SESSION:0.1 '#roscore' C-m
+sleep 1
+
+# Start the rosnode pyserial
+tmux send-keys -t $SESSION:0.2 'cd ~/catkin_ws/' C-m
+tmux send-keys -t $SESSION:0.2 'source ~/catkin_ws/devel/setup.bash' C-m
+tmux send-keys -t $SESSION:0.2 'rosrun rosserial_arduino serial_node.py _port:=/dev/ttyArduino' C-m
+
+# Put kill sess ready to go
+tmux send-keys -t $SESSION:0.3 'tmux kill-session'
+
+# Open the session up
+tmux -2 attach-session -t $SESSION:0
+
+# Need it to run the command << source-file ~/.tmux.conf (from https://www.linode.com/docs/networking/ssh/persistent-terminal-sessions-with-tmux/)>>
+
+
+
+# Kill the session
+# tmux kill-session
